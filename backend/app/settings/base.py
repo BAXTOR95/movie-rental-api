d6c09080f763f90121d8bea8c25f97db806b3f94
Brian Arriaga <brian.arriaga@gmail.com>
2021-05-03 17:04:07 -0400

    Added cloudinary to installed apps

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'cloudinary',
     'storages',
     'corsheaders',
     'rest_framework',
--- backend/app/settings/production.py
+++ backend/app/settings/production.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import dj_database_url
+import cloudinary
 
 from app.settings.base import *
 
@@ -21,11 +22,17 @@ if "DATABASE_URL" in env:
 
 # Cloudinary
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
-    'API_KEY': os.environ.get('API_KEY'),
-    'API_SECRET': os.environ.get('API_SECRET')
-}
+# CLOUDINARY_STORAGE = {
+#     'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+#     'API_KEY': os.environ.get('API_KEY'),
+#     'API_SECRET': os.environ.get('API_SECRET')
+# }
+
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUD_NAME'),
+    api_key=os.environ.get('API_KEY'),
+    api_secret=os.environ.get('API_SECRET')
+)
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
